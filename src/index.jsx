8e559e291ef773333c04991cd05acc83d8434d7d
Politova <anna_loginovskikh@mail.ru>
2025-04-15 15:45:27 +0500

    add lint

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import React from 'react';
+import './style.css';
 
 import Main from './Main.jsx';
-
 const root = ReactDOM.createRoot(document.getElementById('app'));
 root.render(<Main/>);
